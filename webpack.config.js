679fea84cf3c3cdb1daaf15d39081775ca6bcbdf
Orekhova <irinamarkina803@gmail.com>
2024-12-03 13:25:32 +0400

    dev-server changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
